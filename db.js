1f2a7291d552869f001c4fc7be748deba48a1d48
Lucas25 <lucas@it-up.com.ar>
2023-08-11 11:15:23 -0300

    last

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 //const localDB = 'mongodb://127.0.0.1:27017/db';
-const localDB = "mongodb://atlas-sql-64d6323baced724294abf7b2-swydi.a.query.mongodb.net/Cluster0?ssl=true&authSource=admin";
+const localDB = "mongodb://atlas-sql-64d6323baced724294abf7b2-swydi.a.query.mongodb.net/rich-gray-bream-cuffCyclicDB?ssl=true&authSource=admin";
 
 
     const connectDB = async () => {
