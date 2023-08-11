a1f1b302cfa68f8756338001e9bf7ab8cfb870d3
Lucas25 <lucas@it-up.com.ar>
2023-08-11 10:46:30 -0300

    last

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 //const localDB = 'mongodb://127.0.0.1:27017/db';
-const localDB = 'mongodb+srv://lucaspupillilp:sH6-xzxdGmsSBmw@cluster0.fdpf52b.mongodb.net/rich-gray-bream-cuffCyclicDB';
+const localDB = 'mongodb+srv://lucaspupillilp:sH6-xzxdGmsSBmw@cluster0.fdpf52b.mongodb.net/Cluster0';
 
 const connectDB = async () => {
     try {
