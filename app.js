20570d5f96dad8623b2959a4fac4be6a70507cce
Lucas25 <lucas@it-up.com.ar>
2023-08-13 21:03:22 -0300

    last

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3001;
+const PORT = 3000;
 const cors = require("cors");
 const dataRoutes = require('./src/routes/dataRoutes'); // Importa las rutas de datos
 const path = require('path');
