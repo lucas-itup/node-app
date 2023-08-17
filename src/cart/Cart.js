819152873e0fd040a01be426f948020f18977f13
Lucas25 <lucas@it-up.com.ar>
2023-08-17 20:53:20 -0300

    last

--- src/cart/Cart.js
+++ src/cart/Cart.js
@@ -10,8 +10,10 @@ exports.getCart = async (req, res) => {
 };
 
 exports.addToCart = async (req, res) => {
-    const userId = req.body; // Obtén userId del cuerpo de la solicitud
+    const userId = req.body.userId; // Obtén userId del cuerpo de la solicitud
     console.log(req.body.userId); // Verifica qué se está recibiendo en el cuerpo de la solicitud
+    console.log(req.body); // Verifica qué se está recibiendo en el cuerpo de la solicitud
+    console.log("hola"); // Verifica qué se está recibiendo en el cuerpo de la solicitud
     try {
         const cart = await Cart.findOneAndUpdate(
             { userId },
